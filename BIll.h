2a417570158252f999614f9db063186a2f43dd00
Isha Singh <ishaxplore2315@gmail.com>
2026-08-30 11:55:43 +0530

    Update code

--- BIll.h
+++ BIll.h
@@ -1,8 +1,25 @@
+
+#ifndef BILL_H
 #define BILL_H
-#include "MENU.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Forward declaration of Menu class (agar alag se define kiya hai)
+class Menu;
+
 class Bill {
-    static int nextBillNo;   
+    static int nextBillNo;   // static member
     int BILLNO;
     int CUSTOMERID;
-    Menu items[30];
+    Menu items[30];          // array of Menu items
+
+public:
+    Bill(int custID = 0);    // constructor declaration
+
+    void readCustomerID();   // function to take input
+    void show();             // function to display details
 };
+
+#endif
