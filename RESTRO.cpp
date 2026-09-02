1a44eb4cb7e5bc3254397a2d963f0f26d12a0b5f
Isha Singh <ishaxplore2315@gmail.com>
2026-09-02 16:45:07 +0530

    updated a restro.cpp

--- RESTRO.cpp
+++ RESTRO.cpp
@@ -8,7 +8,6 @@ class Restro {
 private:
     string RESTRONAME;
     string OWNERNAME;
-
 public:
     Restro() {
         cout << "ENTER RESTRO NAME:";
@@ -21,8 +20,45 @@ public:
         cout << "---- RESTRO DETAILS ----\n";
         cout << "RESTRONAME: " << RESTRONAME << endl;
         cout << "OWNERNAME:" << OWNERNAME << endl;
+
     }
+         void showDashboard() {
+        cout << "-------------------------------------\n";
+        cout << "         WELCOME TO " << RESTRONAME << "\n";
+        cout << "         Founder: " << OWNERNAME << "\n";
+        cout << "--------------------------------------\n";
+        cout << "              DASHBOARD              \n";
+        cout << "-------------------------------------\n";
+         int choice;
+        cout << "\n----Restaurant Dashboard----\n";
+        cout << "1. Tables\n";
+        cout << "2. Menu\n";
+        cout << "3. Bill\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+                cout << "\n--- Tables Section ---\n";
+                cout<< "Enter the number of tables: ";
+                break;
+            case 2:
+                cout << "\n--- Menu Section ---\n";    
+                cout << "Enter the number of menu items: ";            
+                break;
+            case 3:
+                cout << "\n--- Bill Section ---\n";   
+                cout<<"Enter the number of bills: ";             
+                break;
+            case 4:
+                cout << "\nExiting Dashboard... Thank you!\n";
+        
+                break;   
+        } 
+    }                   
 };
+
 
 int main() {
     Restro r;
